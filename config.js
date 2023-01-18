758141962ae6cc69866c4d1eb0578adff9108e35
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2023-01-19 02:32:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
 module.exports = {
-    config:function(){
-        return (
-            {
-                adminUsers:[ADMIN_ID], //admin users' telegram id, should be numbers
-                botToken: 'YOUR_BOT_TOEKN', // bot token
+    config: function() {
+        if (typeof process.env.ADMIN_ID === "undefined") {
+            console.error("Error: ADMIN_ID environment variable is not defined.");
+            return;
+        }
+        if (typeof process.env.BOT_TOKEN === "undefined") {
+            console.error("Error: BOT_TOKEN environment variable is not defined.");
+            return;
+        }
+        const adminId = Number(process.env.ADMIN_ID);
+        if (isNaN(adminId)) {
+            console.error("Error: ADMIN_ID environment variable is not a valid number.");
+            return;
+        }
 
-            }
-        );
+        return {
+            adminUsers: [adminId],
+            botToken: process.env.BOT_TOKEN
+        }
     }
 };
