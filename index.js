b90773b8406a7723740883560894f2ecd398acd8
Dazzler555 <71560605+Dazzler555@users.noreply.github.com>
2023-01-19 11:49:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,6 @@ const port = process.env.PORT || 3000
 const url = process.env.URL
 const cron = require('node-cron');
 const https = require('https');
-const { fork } = require('child_process');
-
-const appFunction = () => {
-    const app = express();
-    app.get('/', (req, res) => {
-        res.send('Server is running!');
-    });
-
-    cron.schedule('* * * * *', () => {
-        https.get(url, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                console.log(`Server pinged at ${new Date()}. Response: ${data}`);
-            });
-        }).on("error", (err) => {
-            console.log(`Error: ${err.message}`);
-        });
-    });
-
-    const server = app.listen(7000, () => {
-        console.log("Server running");
-    });
-}
 
 
 
